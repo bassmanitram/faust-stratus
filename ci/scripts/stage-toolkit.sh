015fa100e76f8b000f38d8703f0c36e3bb945204
Martin Bartlett <jbartle9@dxc.com>
2024-05-27 16:59:20 +0200

    Fix CI

--- ci/scripts/stage-toolkit.sh
+++ ci/scripts/stage-toolkit.sh
@@ -2,22 +2,11 @@
 # build the install package
 #
 OLDDIR=$PWD
-WORKDIR=$(mktemp -d)
-
-##
-## Install cleanup routine
-##
-for sig in SIGTERM ERR EXIT; do trap "_term ${sig}" ${sig}; done
-_term() {
-  ${OLDDIR}
-  trap "" SIGTERM ERR EXIT
-  rm -rf ${WORKDIR}
-}
+WORKDIR=build
+mkdir ${WORKDIR}
 
 cd ${WORKDIR}
 cp -pr $OLDDIR/srcs/stratus-python .
 cp -pr $OLDDIR/srcs/faust-stratus .
 cp -pr $OLDDIR//tests .
 cp -pr $OLDDIR/srcs/install/* .
-
-zip -r ${OLDDIR}/faust-stratus.zip ./*
